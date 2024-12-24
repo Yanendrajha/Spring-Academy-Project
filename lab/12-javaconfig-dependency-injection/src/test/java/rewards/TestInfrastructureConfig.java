1c1c6909c10f2bc087cc933da20ee197985197e4
Yanendrajha <yanendrajha37@gmail.com>
2024-12-24 17:45:12 +0530

    Done the 2nd & 3rd  module : Configuration of Java

--- lab/12-javaconfig-dependency-injection/src/test/java/rewards/TestInfrastructureConfig.java
+++ lab/12-javaconfig-dependency-injection/src/test/java/rewards/TestInfrastructureConfig.java
@@ -1,7 +1,9 @@
 package rewards;
 
+import config.RewardsConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
 import javax.sql.DataSource;
@@ -53,8 +55,9 @@ import javax.sql.DataSource;
  *
  */
 @Configuration
-public class TestInfrastructureConfig {
+@Import(RewardsConfig.class)
 
+public class TestInfrastructureConfig {
 	/**
 	 * Creates an in-memory "rewards" database populated
 	 * with test data for fast testing
